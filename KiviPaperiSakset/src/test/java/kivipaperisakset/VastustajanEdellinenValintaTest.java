f39906a0d2a2a84c6edb46f325e38d820fb4769c
sovu <sonja.vuorinen@helsinki.fi>
2019-01-24 21:08:59 +0200

    testausta

---
+++ KiviPaperiSakset/src/test/java/kivipaperisakset/VastustajanEdellinenValintaTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kivipaperisakset;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sovu
+ */
+public class VastustajanEdellinenValintaTest {
+    VastustajanEdellinenValinta vastustaja;
+    
+    public VastustajanEdellinenValintaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        vastustaja = new VastustajanEdellinenValinta();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void valitseeAluksiSakset() {
+        assertTrue(vastustaja.valitse() == 2);
+    }
+    
+    @Test
+    public void valitseePaperinVastaanottamisenJalkeenSakset() {
+        vastustaja.vastaanotaValinta(1);
+        assertTrue(vastustaja.valitse() == 2);
+    }
+    
+    @Test
+    public void valitseeSaksienVastaanottamisenJalkeenKiven() {
+        vastustaja.vastaanotaValinta(2);
+        assertTrue(vastustaja.valitse() == 0);
+    }
+    
+    @Test
+    public void valitseeKivenVastaanottamisenJalkeenPaperin() {
+        vastustaja.vastaanotaValinta(0);
+        assertTrue(vastustaja.valitse() == 1);
+    }
+}
